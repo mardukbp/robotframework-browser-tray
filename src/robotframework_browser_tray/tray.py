7375e2ef644fc690de4471e09e72d1bc0536f99c
Marduk Bolaños <marduk.bolanos@imbus.de>
2024-01-12 12:45:48 +0100

    - Updated README - Added optional arguments for playwright port and chromium port

--- src/robotframework_browser_tray/tray.py
+++ src/robotframework_browser_tray/tray.py
@@ -1,18 +1,14 @@
+import argparse
 import os
 import site
+import subprocess
 import sys
 from os.path import realpath
 from pathlib import Path
 from subprocess import Popen
-from tkinter import messagebox
 from PIL import Image
 from pystray import Icon, Menu, MenuItem
 
-
-REMOTE_DEBUGGING_PORT = 1234
-PLAYWRIGHT_PROCESS_PORT= 55555
-
-chromium_icon = Image.open(Path(realpath(__file__)).parent / "chromium.png")
 
 _, site_packages = site.getsitepackages()
 browser_wrapper = Path(site_packages) / "Browser" / "wrapper"
@@ -20,87 +16,111 @@ index_js = browser_wrapper / "index.js"
 node_modules = browser_wrapper / "node_modules"
 local_browsers = node_modules / "playwright-core" / ".local-browsers"
 
-processes: list[Popen] = []
 
-
-def find_chromium(path: Path) -> Path:
-    chromium = [
-        file.path
+def find_chromium(path: Path) -> list[Path]:
+    return [
+        Path(file.path)
         for file in os.scandir(path)
         if file.is_dir() and file.name.startswith("chromium")
     ]
 
-    if chromium:
-        return Path(chromium[0])
-    
-    return None
-
 
 def assert_playwright_initialized():
     if not (node_modules.is_dir() and local_browsers.is_dir() and find_chromium(local_browsers)):
-        messagebox.showerror("Playwright wrapper error", "\n\n".join([
-            "Playwright has not been initialized.",
-            "Execute 'rfbrowser init chromium'."
-        ]))
-        sys.exit(1)
+       raise FileNotFoundError("Playwright has not been initialized. Execute 'rfbrowser init chromium'.")
 
 
-def start_playwright():
-    try:
-        node_process = Popen(f"node {index_js} {PLAYWRIGHT_PROCESS_PORT}")
-        processes.append(node_process)
-    except FileNotFoundError as error:
-        messagebox.showerror("Playwright wrapper could not be started", error)
+def start_playwright(playwright_process_port: int) -> Popen:
+    return Popen(f"node {index_js} {playwright_process_port}")
 
 
-def start_chromium(incognito=False):
+def start_chromium(remote_debugging_port: int, incognito=False) -> Popen:
     incognito_flag = "-incognito" if incognito else ""
-
-    try:
-        chrome_exe = find_chromium(local_browsers) / "chrome-win" / "chrome.exe"
-        chrome_process = Popen(f"{chrome_exe} --remote-debugging-port={REMOTE_DEBUGGING_PORT} --test-type {incognito_flag}")
-        processes.append(chrome_process)
-    except FileNotFoundError as error:
-        messagebox.showerror("Chromium could not be started", error)
+    chromium_dir = find_chromium(local_browsers)[0]
+    chrome_exe = chromium_dir / "chrome-win" / "chrome.exe"
+    return Popen(f"{chrome_exe} --remote-debugging-port={remote_debugging_port} --test-type {incognito_flag}")
 
 
-def open_chromium():
-    start_chromium()
+def new_tray_icon(processes: list[Popen], remote_debugging_port: int) -> Icon:
+    def exit():
+        for proc in processes:
+            proc.terminate()
 
+        icon.stop()
 
-def open_chromium_incognito():
-    start_chromium(True)
+    def open_chromium():
+        chromium = start_chromium(remote_debugging_port)
+        processes.append(chromium)
 
+    def open_chromium_incognito():
+        chromium = start_chromium(remote_debugging_port, True)
+        processes.append(chromium)
 
-def exit():
-    for proc in processes:
-        proc.terminate()
+    chromium_icon = Image.open(Path(realpath(__file__)).parent / "chromium.png")
 
-    icon.stop()
-
-
-icon = Icon(
-    name='Browser Tray',
-    icon=chromium_icon,
-    menu=Menu(
-        MenuItem(
-            'Open Chromium',
-            open_chromium,
-            default=True
-        ),        
-        MenuItem(
-            'Open Chromium Incognito',
-            open_chromium_incognito,
-        ),
-        MenuItem(
-            'Exit',
-            exit
+    icon = Icon(
+        name='Browser Tray',
+        icon=chromium_icon,
+        menu=Menu(
+            MenuItem(
+                'Open Chromium',
+                open_chromium,
+                default=True
+            ),        
+            MenuItem(
+                'Open Chromium Incognito',
+                open_chromium_incognito,
+            ),
+            MenuItem(
+                'Exit',
+                exit
+            )
         )
     )
-)
+
+    return icon
+
+
+def is_running(cmd: str) -> bool:
+    process_list = subprocess.check_output(
+        ["TASKLIST", "/FI", f'imagename eq {cmd}'],
+        encoding="utf-8"
+    ).splitlines()
+    instances = [
+        process 
+        for process in process_list 
+        if process.startswith(cmd)
+    ]
+    return len(instances) > 1
+
+
+def get_ports() -> tuple[int, int]:
+    MAX_PORT = 2**16
+    REMOTE_DEBUGGING_PORT = 1234
+    PLAYWRIGHT_PROCESS_PORT= 55555
+    
+    arg_parser = argparse.ArgumentParser(add_help=True)
+    arg_parser.add_argument("--pw-port", default=PLAYWRIGHT_PROCESS_PORT, type=int, help=f"Playwright process port (default: {PLAYWRIGHT_PROCESS_PORT})")
+    arg_parser.add_argument("--cdp-port", default=REMOTE_DEBUGGING_PORT, type=int, help=f"Chromium debugging port (default: {REMOTE_DEBUGGING_PORT})")
+    args = arg_parser.parse_args()
+    playwright_process_port = args.pw_port
+    remote_debugging_port = args.cdp_port
+
+    if playwright_process_port > MAX_PORT or remote_debugging_port > MAX_PORT:
+        raise ValueError(f"Port numbers cannot be larger than {MAX_PORT}")
+
+    return (playwright_process_port, remote_debugging_port)
 
 
 def run():
+    cmd = "browser-tray.exe"
+    if is_running(cmd):
+        print(f"{cmd} is already running")
+        sys.exit(1)
+
+    playwright_process_port, remote_debugging_port = get_ports()
+
     assert_playwright_initialized()
-    start_playwright()
-    icon.run()
+    playwright_process = start_playwright(playwright_process_port)
+    tray_icon = new_tray_icon([playwright_process], remote_debugging_port)
+    tray_icon.run()
