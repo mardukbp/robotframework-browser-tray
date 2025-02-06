918ea5e631448575251908273f219025cb24b661
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-06 16:22:01 +0100

    During test setup set the npm cache directory to a writeable directory and clean up the node_modules in Playwright wrapper

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,6 @@ import subprocess
 
 @pytest.fixture(scope="session", autouse=True)
 def rfbrowser_init():
+    subprocess.run("npm.cmd config set cache C:\\tmp\\nodejs\\npm-cache --global")
+    subprocess.run("rfbrowser clean-node")
     subprocess.run("rfbrowser init chromium")
